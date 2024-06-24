ffdeddc8cba6afae788afdc17a813e6b2d937947
Damien Lando <damien.lando@gmail.com>
2024-06-24 16:56:29 +0200

    1st

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+import json
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
+def load_posts():
+    try:
+        with open('blog_posts.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+
+def save_posts(posts):
+    with open('blog_posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        blog_posts = load_posts()
+        new_id = max(post['id'] for post in blog_posts) + 1 if blog_posts else 1
+        new_post = {'id': new_id, 'author': author, 'title': title, 'content': content}
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
